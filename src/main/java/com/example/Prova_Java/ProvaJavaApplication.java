c2f03f7b0ca8251fed6cfb2d58d1dd9871e24ae3
Gustavo <gustavodossantosguimarae93@gmail.com>
2026-04-08 19:13:18 -0300

    first commit

---
+++ src/main/java/com/example/Prova_Java/ProvaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Prova_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Prova_Java/ProvaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Prova_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
